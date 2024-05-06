43dfcef82feec2792108dafffbc9126a814ca1ab
Olga Blud <olga.blud@gmail.com>
2024-05-06 19:54:25 +0300

    add task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
+const logInForm = document.querySelector('.login-form');
+logInForm.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+  } else {
+    console.log({ email, password });
+  }
+  form.reset();
+}
