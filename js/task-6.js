5b1b983d4b26362a8271683786fa99bb89ce5661
Olga Blud <olga.blud@gmail.com>
2024-05-08 09:37:00 +0300

    chaged task 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,39 +1,38 @@
 const input = document.querySelector('input');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
 
 input.addEventListener('change', setInputValue);
-createBtn.addEventListener('click', createBlocks);
-destroyBtn.addEventListener('click', destroyBlocks);
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
 
 function setInputValue(event) {
   event.currentTarget.value;
 }
-function createBlocks() {
-  if (input.value >= 1 && input.value <= 100) {
-    createBoxes(input.value);
-  }
-}
 
-function destroyBlocks() {
-  document.querySelectorAll('.box').forEach(element => element.remove());
+function destroyBoxes() {
+  // document.querySelectorAll('.box').forEach(element => element.remove());
+  boxesContainer.innerHTML = '';
 }
-
 function createBoxes(amount) {
-  const boxesContainer = document.querySelector('#boxes');
-  // boxesContainer.innerHTML = '';
-  destroyBlocks();
+  destroyBoxes();
+  amount = input.value;
   let size = 30;
-  for (let i = 0; i < amount; i++) {
-    const newBox = document.createElement('div');
-    boxesContainer.append(newBox);
-    newBox.classList.add('box');
-    newBox.style.width = `${size}px`;
-    newBox.style.height = `${size}px`;
-    newBox.style.backgroundColor = getRandomHexColor();
-    size += 10;
+  if (amount >= 1 && amount <= 100) {
+    for (let i = 0; i < amount; i++) {
+      const newBox = document.createElement('div');
+      boxesContainer.append(newBox);
+      newBox.classList.add('box');
+      newBox.style.width = `${size}px`;
+      newBox.style.height = `${size}px`;
+      newBox.style.backgroundColor = getRandomHexColor();
+      size += 10;
+    }
   }
+  input.value = '';
 }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
