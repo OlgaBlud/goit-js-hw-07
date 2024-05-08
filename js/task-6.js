93055863669f4cbb48d20dd77ec069554aee5158
Olga Blud <olga.blud@gmail.com>
2024-05-08 08:56:45 +0300

    done task 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,39 @@
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+
+input.addEventListener('change', setInputValue);
+createBtn.addEventListener('click', createBlocks);
+destroyBtn.addEventListener('click', destroyBlocks);
+
+function setInputValue(event) {
+  event.currentTarget.value;
+}
+function createBlocks() {
+  if (input.value >= 1 && input.value <= 100) {
+    createBoxes(input.value);
+  }
+}
+
+function destroyBlocks() {
+  document.querySelectorAll('.box').forEach(element => element.remove());
+}
+
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector('#boxes');
+  // boxesContainer.innerHTML = '';
+  destroyBlocks();
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const newBox = document.createElement('div');
+    boxesContainer.append(newBox);
+    newBox.classList.add('box');
+    newBox.style.width = `${size}px`;
+    newBox.style.height = `${size}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+    size += 10;
+  }
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
